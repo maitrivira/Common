8e1411ed252a9774342b64117b1db4f0a24b845b
Maitri Vira <maitrivira2@gmail.com>
2022-11-06 16:13:00 +0700

    localization

---
+++ Sources/Common/Extension/LocalizedLang.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by Maitri Vira on 06/11/22.
+//
+
+import Foundation
+
+public struct LocalizedLang {
+    public static let resto = "resto".localized()
+    public static let favorite = "favorite".localized()
+    public static let search = "search".localized()
+    public static let profile = "profile".localized()
+    public static let empty = "empty_search".localized()
+    public static let notfound = "not_found".localized()
+    public static let emptyFav = "empty_fav".localized()
+    public static let desc = "desc".localized()
+    public static let exp = "exp".localized()
+    public static let food = "food".localized()
+    public static let drink = "drink".localized()
+    public static let reviews = "reviews".localized()
+    public static let rating = "rating".localized()
+}
